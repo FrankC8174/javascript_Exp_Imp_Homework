6d33b93b833c6e8e9d8fb0d9096b94140b652a50
Franklin Simpson <simpson.franklin44@gmail.com>
2022-03-31 21:07:01 -0400

    Initial class lecture commit

---
+++ export_import_lesson/main.js
@@ -0,0 +1,81 @@
+import {API_KEY, upperCaseName} from "./wordPlay.js";
+
+// const names = [
+//     "Frank",
+//     "Gabriel",
+//     "Awa"
+// ];
+
+// names.forEach((name) => {
+//     snooperizer(name);
+// })
+console.log(API_KEY);
+console.log(uppercaseName("Frank"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  Array destructuring
+
+// const names = ["troy", "Troy2", "Troy3", "troy4", "troy5"];
+
+// const [firstName, secondName] = names;
+
+// console.log(secondName);
+
+
+// Object Destructuring
+// const player = {
+//     name: "Frank",
+//     age: Infinity,
+//     location: "Atlanta",
+//     sport: "Life,"
+
+// }
+// let {name, sport} = player;
+// console.log(sport);
+
+// sport = "coding";
+// console.log(player);
+// console.log(sport);
+
+//::::::::::::::::::::::::::::::::::::: Export Import:::::::::::::::::::::::::::::::::::::::
+
+
+
---
+++ export_import_lesson/wordPlay.js
@@ -0,0 +1,17 @@
+export  function snooperizer(name) {
+    console.log(`${name}izzle`);
+}
+
+export const API_KEY = "LLLAL1L1222";
+
+
+export function uppercaseName(name){
+    console.log(name.toUpperCase())
+
+} 
+
+export const powers = {
+    jump: 10,
+    punch: 20,
+    swim: 50
+};
